6b6270259f71fab763e54ea1d0dcb8a5c99c22ae
Sidd-dmg <102126686+Sidd-dmg@users.noreply.github.com>
2023-01-07 09:27:06 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -10,7 +10,7 @@ _pltdata = pd.read_csv('_week2.csv',sep=",")
 
 
 app.layout = html.Div([
-    html.H4('Data Analysis of Electricity Consumption. '),
+    html.H1('Data Analysis of Electricity Consumption. '),
     dcc.Dropdown(_pltdata.columns, id='dropdown'),
      dcc.Graph(id="graph"),
     #html.Div(id='pandas-output-container-1')
@@ -28,8 +28,7 @@ def update_line_chart(val_chosen):
     fig = px.line(pltdata, x='time', y=para)
     fig.update_layout(
                    plot_bgcolor='rgb(230, 230,230)',
-                   showlegend=True,
-                   height=800, width= 1900)
+                   showlegend=True)
     return fig
 
 if __name__ == "__main__":
