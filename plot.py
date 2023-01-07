9a8c553af267194c3c1919f7d2f284c89bf51056
Sidd-dmg <102126686+Sidd-dmg@users.noreply.github.com>
2023-01-07 11:30:21 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,16 +3,18 @@ import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
-server = app.server
 
-_pltdata = pd.read_csv('_week2.csv',sep=",")
+#_pltdata = pd.read_csv('D:\\Project_TeamPower\\webdevelopment\\New folder\\_week2.csv',sep=",")
 
-
+_pltdata = ['Ptot','Pr','Py','Pb','Pfavg','Pfr','Pfy','Pfb','St','Sr','Sy','Sb','Vlavg','Vry','Vyb','Vbr','Vlnavg','Vr','Vy','Vb','Itot','Ir','Iy','Ib','freq','Energy','vah']
 
 app.layout = html.Div([
     html.H1('Data Analysis of Electricity Consumption. '),
-    dcc.Dropdown(_pltdata.columns, id='dropdown'),
-     dcc.Graph(id="graph"),
+    html.I("Select the date for ploting.[format:YYYY-MM-DD]"),
+    html.Br(),
+    dcc.Input(id="dates", type="text", placeholder=""),
+    dcc.Dropdown(_pltdata, id='dropdown'),
+    dcc.Graph(id="graph"),
     #html.Div(id='pandas-output-container-1')
 ])
 
@@ -20,17 +22,33 @@ app.layout = html.Div([
 
 @app.callback(
     Output("graph", "figure"), 
-    Input("dropdown", "value"))
-def update_line_chart(val_chosen):
-    pltdata = pd.read_csv('_week2.csv',sep=',') # replace with your own data source
-    para=val_chosen
+    Input("dropdown", "value"),
+    Input("dates","value"))
+def update_line_chart(dropdown,dates):
+    push_date = "indouk_4_"+str(dates)+".txt"
+
+    path= "/data/" + push_date # Plz add github file locatio path in "".in this kind of format 'D:\\Project_TeamPower\\webdevelopment\\New folder\\_week2.csv'
+
+    data_write = 'time,meter,Ptot,Pr,Py,Pb,Pfavg,Pfr,Pfy,Pfb,St,Sr,Sy,Sb,Vlavg,Vry,Vyb,Vbr,Vlnavg,Vr,Vy,Vb,Itot,Ir,Iy,Ib,freq,Energy,vah'
+
+    output_file = open('Data.csv','w')
+    with open(path,'r') as scan:
+       output_file.write(data_write+'\n')
+       output_file.write(scan.read())
+    output_file.close()
+   
+    data_path='Data.csv' # here add  the path of output file generated in "" similar to 'D:\\Project_TeamPower\\webdevelopment\\New folder\\_week2.csv' format
+
+    pltdata = pd.read_csv(data_path,sep=',') # replace with your own data source
+    para=dropdown
     print(para)
     fig = px.line(pltdata, x='time', y=para)
     fig.update_layout(
+                  #plot_bgcolor='rgb(230, 230,230),
                    showlegend=True,
-                   height = 800)
+                   height=800)
     return fig
 
-if __name__ == "__main__":
-    app.run_server(debug=False)
+
+app.run_server(debug=True)
 
